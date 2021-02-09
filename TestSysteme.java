99dc1d95c040ba304b8e5de4e197fa6de7dc96b0
Steven <steven928@hotmail.fr>
2021-02-09 16:25:12 +0100

    issou

--- TestSysteme.java
+++ TestSysteme.java
@@ -39,6 +39,8 @@ public class TestSysteme {
 	 * Si besoin ajouter une methode avec l'annotation @After pour executer une
 	 * methode apres chaque test
 	 */
+	
+	 // TESTS INVARIANTS
 
 	/* Test qui doit se terminer sans levee d'exception */
 	@Test
@@ -50,7 +52,7 @@ public class TestSysteme {
 
 	// > si current est null, alors last est null et waiting est vide.
 	@Test
-	public void testCurrent() throws ErreurSysteme {
+	public void testCurrentNull() throws ErreurSysteme {
 		if (sys.isCurrent(null)) {
 			assertTrue(sys.getWaiting().isEmpty());
 		}
@@ -86,13 +88,81 @@ public class TestSysteme {
 		sys.add(p1);
 		assertTrue(sys.isCurrent(p1));
 	}
+	
+	// TESTS SWAP
 
+	/* > Swap lève l'exception ErreurSystème (sous-classe de Exception) s’il existe n'existe
+aucun processus en attente de la ressource et laisse l'état inchangé. */
 	@Test(expected = ErreurSysteme.class)
 	public void testNoProcessusWaiting() throws ErreurSysteme{
 		sys.add(p1);
 		sys.swap();
 	}
 	
+	/* À la sortie de swap ce n’est plus le processus actuel qui dispose de la ressource, le
+possesseur à l’entrée a rejoint la liste des demandeurs et on se souvient que c’est lui qui
+détenait la ressource au moment de l’échange. Si on a le choix on attribue la ressource à un
+processus autre que celui qui l’avait lors de l’échange précédent. Le système peut choisir
+n’importe quel processus demandeur parmi ceux qui respectent les contraintes précédentes.*/
+	@Test
+	public void testCurrentAfterSwap() throws ErreurSysteme{
+		sys.add(p1);
+		sys.add(p2);
+		sys.swap();
+		assertTrue(sys.isCurrent(p2));
+		assertTrue(sys.isLast(p1));
+		assertTrue(sys.getWaiting().contains(p1));
+		assertFalse(sys.getWaiting().contains(p2));
+	}
+
+	@Test(expected = ErreurSysteme.class)
+	public void testAddSameProc() throws ErreurSysteme{
+		sys.add(p1);
+		sys.add(p1);
+	}
+
+
+	@Test
+	public void testNumberWaiting() throws ErreurSysteme{
+		sys.add(p1);
+		sys.add(p2);
+		
+		assertEquals(sys.getWaiting().size(), 1);
+		sys.swap();
+		assertEquals(sys.getWaiting().size(), 1);
+		sys.swap();
+		assertEquals(sys.getWaiting().size(), 1);
+
+		sys.add(p3);
+		
+		assertEquals(sys.getWaiting().size(), 2);
+		sys.swap();
+		assertEquals(sys.getWaiting().size(), 2);
+		sys.swap();
+		assertEquals(sys.getWaiting().size(), 2);
+	}
+
+	// TESTS SYSTEME
+
+	/* renvoie le processus qui détient actuellement la ressource ou
+lève l'exception ErreurSysteme s'il n'existe aucun tel processus. */
+	@Test(expected = ErreurSysteme.class)
+	public void testNoCurrent() throws ErreurSysteme{
+		sys.current();
+	}
+	
+	@Test
+	public void testCurrent() throws ErreurSysteme{
+		sys.add(p1);
+		assertEquals(sys.current(), p1);
+	}
+
+
+
+
+
+
+
 
 	public static junit.framework.Test suite() {
 		return new JUnit4TestAdapter(TestSysteme.class);
