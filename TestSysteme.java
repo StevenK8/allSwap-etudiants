5a384ee580d6dcf51cd467758a9cbbb2591cc434
Steven <steven928@hotmail.fr>
2021-02-09 15:06:07 +0100

    testCurrent

--- TestSysteme.java
+++ TestSysteme.java
@@ -7,7 +7,8 @@
 /* Il y a d'autre sortes d'assert, si besoin */
 	import static org.junit.Assert.assertTrue;
 	import static org.junit.Assert.assertFalse;
-	import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertEquals;
 	import junit.framework.JUnit4TestAdapter;
 	
 	public class TestSysteme { 
@@ -41,6 +42,12 @@
 	    	sys.init();
 	    	
 	    }
+
+		@Test public void testCurrent()throws ErreurSysteme {
+			if(sys.current()==null){
+				assertTrue(sys.getWaiting().isEmpty());
+			}
+		}
 	
 
 	    /* Test dont l'excution doit lever l'exception indiquee. */
