14ef0d47fa39d7b236cdb54e5d46928fc5e6d3e8
dam <damien.chancerel@gmail.com>
2021-02-09 14:55:17 +0100

    bonjour

--- TestSysteme.java
+++ TestSysteme.java
@@ -11,6 +11,14 @@
 	import junit.framework.JUnit4TestAdapter;
 	
 	public class TestSysteme { 
+		
+		
+		Systeme sys;
+		Processus p1;
+		Processus p2;
+		Processus p3;
+		Processus p4;
+		Processus p5;
 	
 	    /* Executee avant chaque methode de test. */
 	    @Before public void setUp () {
@@ -22,12 +30,28 @@
 	
 	    /* Test qui doit se terminer sans levee d'exception */
 	    @Test public void test1() { 
+	    
+	    	sys = new Systeme();
+	    	p1 = new Processus("proc1");
+	    	p1 = new Processus("proc2");
+	    	p1 = new Processus("proc3");
+	    	p1 = new Processus("proc4");
+	    	p1 = new Processus("proc5");
+	    	
+	    	sys.init();
+	    	
 	    }
 	
 
 	    /* Test dont l'excution doit lever l'exception indiquee. */
 	    @Test(expected=ErreurSysteme.class)
-	    public void test2() throws ErreurSysteme { }
+	    public void test2() throws ErreurSysteme {
+	    	
+	    	assertTrue(sys.current().equals(null));
+	    	sys.add(p1);
+	    	assertTrue(sys.current().equals(p1));
+	    	
+	    }
 
 
 	    public static junit.framework.Test suite() {
